3419a9b13285a4a4cfe4907871c5ceacadc64fe0
Dmytro Potapenko <dima.lmkh@gmail.com>
2023-03-21 22:03:17 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,13 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Stat/Statistics';
-import { FriendList } from './Friends/FriendList';
-import { TransactionHistory } from './Trans/TransactionHistory';
+// import { Profile } from './Profile/Profile';
+// import { Statistics } from './Stat/Statistics';
+// import { FriendList } from './Friends/FriendList';
+// import { TransactionHistory } from './Trans/TransactionHistory';
 
-import user from '../Data/user.json';
-import data from '../Data/data.json';
-import friends from '../Data/friends.json';
-import transactions from '../Data/transactions.json';
+// import user from '../Data/user.json';
+// import data from '../Data/data.json';
+// import friends from '../Data/friends.json';
+// import transactions from '../Data/transactions.json';
 
 export const App = () => {
-  return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
-      <FriendList prop={friends} />
-      <TransactionHistory prop={transactions} />
-    </div>
-  );
+  return <div>1</div>;
 };
